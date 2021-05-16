9900c2368fe2c87e5873271adad7f77d6179bcf4
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-16 21:26:58 +0600

    oauth TOKEN typo fixed

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
 import { Router } from "../dependencies.js";
 import { OAuth2Client } from "../dependencies.js";
 
-const CLIENT_ID     = Deno.env.toObject().GITHUB_OAUTH_CLIENT_ID;
-const CLIENT_SECRET = Deno.env.toObject().GITHUB_OAUTH_CLIENT_SECRET;
+const GITHUB_OAUTH_CLIENT_ID     = Deno.env.toObject().GITHUB_OAUTH_CLIENT_ID;
+const GITHUB_OAUTH_CLIENT_SECRET = Deno.env.toObject().GITHUB_OAUTH_CLIENT_SECRET;
 
 const oauth2Client = new OAuth2Client({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
+  clientId: GITHUB_OAUTH_CLIENT_ID,
+  clientSecret: GITHUB_OAUTH_CLIENT_SECRET,
   authorizationEndpointUri: "https://github.com/login/oauth/authorize",
   tokenUri: "https://github.com/login/oauth/access_token",
   redirectUri: "https://deno-crypto-payments.herokuapp.com/oauth2/callback",
