5002d55195fbd384513b487496fc1bccf82231ed
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-18 19:46:42 +0600

    mongodb 20.0 version downgraded

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ import { MongoClient } from "./dependencies.js"
 // import { config }      from "./dependencies.js";
 
 const client = new MongoClient();
-await client.connect(Deno.env.toObject().MONGODB_CONNECTION_KEY);
+await client.connectWithUri(Deno.env.toObject().MONGODB_CONNECTION_KEY);
 console.log("Succesfully connected to MongoDB");
 const database = client.database("deno-crypto-payments");
 
