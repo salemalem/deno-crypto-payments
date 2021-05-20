cf0f3e1510dcc0afbfa391b1df9dc89eb2a99027
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-20 12:13:01 +0600

    body()

--- routes/router.js
+++ routes/router.js
@@ -90,7 +90,7 @@ router
     // context.response.redirect("/");
   })
   .post("/login_test", async (context) => {
-    console.log(context.request.serverRequest());
+    console.log(context.request.body());
     // const form = JSON.stringify(await multiParser(context.request.serverRequest));
     // const parse = JSON.parse(form);
     // console.log(parse["fields"]["username"]);
