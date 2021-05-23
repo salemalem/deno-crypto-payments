d4ba9470af14f64f60c14198e43809fc0c3df371
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-23 23:46:34 +0600

    try catch added

--- routes/router.js
+++ routes/router.js
@@ -121,12 +121,12 @@ router
   .get("/seller/:githubID", async (context) => {
     const { githubID } = helpers.getQuery(context, { mergeParams: true });
     const {rows: seller} = await mysqlClient.execute(`SELECT name FROM users WHERE githubID=${githubID}`);
-    context.response.body = seller;
-    // try {
-      // context.response.body = seller["name"];
-    // } catch (error) {
-      // console.log(error);
-    // }
+    // context.response.body = seller;
+    try {
+      context.response.body = seller["name"];
+    } catch (error) {
+      console.log(error);
+    }
     // context.render()
   });
 
