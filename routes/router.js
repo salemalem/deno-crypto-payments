3396372939733515d597d7732a0bfe321d0481a0
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-25 13:44:42 +0600

    single seller name added

--- routes/router.js
+++ routes/router.js
@@ -124,9 +124,9 @@ router
     if(!seller.length) {
       context.response.body = "404 Seller not found";
     } else{
-      // seller[0]["name"];
       const {rows: uploads} = await mysqlClient.execute(`SELECT title, description, trx_amount FROM uploads WHERE githubID=${githubID}`);
       context.render(`${Deno.cwd()}/views/pages/single_seller.ejs`, {
+        name: seller[0]["name"],
         uploads: uploads
       });
     }
