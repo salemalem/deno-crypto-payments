2ff94c7327773b83203b7d15a60b41bac6e44ae6
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-25 14:21:37 +0600

    upload id fix 1

--- static/scripts/single_seller.js
+++ static/scripts/single_seller.js
@@ -1,6 +1,3 @@
-function purchaseProduct(buttonObject) {
-  // parser = new DOMParser();
-  // xmlDoc = parser.parseFromString(String(buttonObject),"text/xml");
-  // console.log(uploadID);
-  console.log(buttonObject[0])
+function purchaseProduct(uploadID) {
+  console.log(uploadID)
 }
