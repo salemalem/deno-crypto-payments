893719f3df589e6311b1613cf60dc0dfe1bebfc1
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-28 00:09:48 +0600

    minor update

--- static/scripts/payment_page.js
+++ static/scripts/payment_page.js
@@ -1,5 +1,6 @@
 let userTronAddress;
 let transactionHash;
+let transactionData;
 
 function proceedPayment() {
   let userTronAddressSelector = document.querySelector("#user-tron-address");
@@ -9,6 +10,8 @@ function proceedPayment() {
 }
 
 function checkHash() {
+  // actually the checking SHOULD be on SERVER side and data should be encrypted during the process 
+  // but since I don't have much time now let's go for this quick way.
   let hashInput = document.querySelector("#transaction-hash");
   transactionHash = hashInput.value;
   $.ajax({
@@ -16,7 +19,8 @@ function checkHash() {
     type: "GET",
     dataType: 'json',
     success: function (data) {
-        console.log(data["contractData"]);
+        transactionData = data["contractData"];
     }
   });
+  // 1 / 1million = amount 1
 }
