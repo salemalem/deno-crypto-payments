1cff200a466a525635c017027f239b6eb93572cb
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-27 23:57:08 +0600

    alert data on json

--- static/scripts/payment_page.js
+++ static/scripts/payment_page.js
@@ -1,4 +1,5 @@
 let userTronAddress;
+let transactionHash;
 
 function proceedPayment() {
   let userTronAddressSelector = document.querySelector("#user-tron-address");
@@ -7,3 +8,16 @@ function proceedPayment() {
   document.querySelector(".payment-step-2").style.visibility = "visible";
 }
 
+function checkHash() {
+  let hashInput = document.querySelector("#transaction-hash");
+  transactionHash = hashInput.value;
+  $.ajax({
+    url: `https://apilist.tronscan.org/api/transaction-info?hash=${transactionHash}`,
+    type: "GET",
+    dataType: 'json',
+    success: function (data) {
+        alert(data);
+        }
+    }
+  });
+}
