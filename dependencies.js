2ecfabacf8b127c74630b7aa32518f24424eedfc
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-27 15:43:25 +0600

    comments added to dependencies

--- dependencies.js
+++ dependencies.js
@@ -17,10 +17,13 @@ export { MongoClient } from "https://deno.land/x/mongo@v0.22.0/mod.ts"
 // environmental variable at .env
 export { config }      from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 
+// for githubOauth
 export { OAuth2Client } from "https://deno.land/x/oauth2_client@v0.2.0/mod.ts";
 
+// to accept --port number from flag since Heroku doesn't allow to set your own port
 export * as flags from "https://deno.land/std@0.97.0/flags/mod.ts";
 
 export { Client as mysqlDriver } from "https://deno.land/x/mysql@v2.9.0/mod.ts";
 
+// checks Directory and creates if it doesn't exist
 export { ensureDir } from "https://deno.land/std@0.97.0/fs/mod.ts";
