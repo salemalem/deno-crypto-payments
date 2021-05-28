12c94a091906bf9a1c7f33cfff9a36d89d152cbf
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-28 22:43:44 +0600

    now payments instead of rows

--- routes/router.js
+++ routes/router.js
@@ -164,8 +164,8 @@ router
   .get("/tools/checkhash/:hash", async (context) => {
     const { hash } = helpers.getQuery(context, { mergeParams: true });
 
-    let {rows}  = await mysqlClient.execute(`SELECT * FROM payments WHERE transactionHash='${hash}'`);
-    console.log(rows);
+    let {rows: payments}  = await mysqlClient.execute(`SELECT * FROM payments WHERE transactionHash='${hash}'`);
+    console.log(payments);
     let jsonBodyOutput;
     if(!payments.length) { // if no payment with this hash was made
       const jsonResult = fetch(`https://apilist.tronscan.org/api/transaction-info?hash=${hash}`);
