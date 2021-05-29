3f694d274679ee01cfe341808323285a5ae39c4e
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-29 22:14:05 +0600

    log expected data

--- static/scripts/payment_page.js
+++ static/scripts/payment_page.js
@@ -1,10 +1,15 @@
 let userTronAddress;
 let transactionHash;
 let transactionData;
+let expectedData = {};
 let status;
 
 function proceedPayment() {
   let userTronAddressSelector = document.querySelector("#user-tron-address");
+  expectedData["upload-id"] = document.querySelector("#upload-id").value;
+  expectedData["github-id"] = document.querySelector("#github-id").value;
+  expectedData["title"] = document.querySelector("#product-title").value;
+  expectedData["seller-name"] = document.querySelector("#seller-name").value;
   userTronAddress = userTronAddressSelector.value;
   userTronAddressSelector.disabled = true;
   document.querySelector(".payment-step-2").style.visibility = "visible";
@@ -14,6 +19,10 @@ function checkHash() {
   // actually the checking SHOULD be on SERVER side and data should be encrypted during the process 
   // but since I don't have much time now let's go for this quick way.
   let hashInput = document.querySelector("#transaction-hash");
+
+  expectedData["seller-tron-address"] = document.querySelector("#seller-tron-address").value;
+  expectedData["trx-amount"] = document.querySelector("#trx-amount").value;
+  
   transactionHash = hashInput.value;
   $.ajax({
     url: `/tools/checkhash/${transactionHash}`,
@@ -38,4 +47,5 @@ function checkHash() {
   });
   // 1 / 1million = amount 1
   console.log(transactionData);
+  console.log(expectedData);
 }
