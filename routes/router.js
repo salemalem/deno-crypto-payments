f962cd66ed3e66d57da8889830b18cc49107f774
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-05-30 23:28:10 +0600

    body() of request

--- routes/router.js
+++ routes/router.js
@@ -209,7 +209,7 @@ router
   })
   .post("/tools/recordHash/:hash", async (context) => {
     const { hash } = helpers.getQuery(context, { mergeParams: true });
-    console.log(context.request);
+    console.log(context.request.body());
     context.response.body = "hi";
   })
   .get("/createTable", async (context) => {
